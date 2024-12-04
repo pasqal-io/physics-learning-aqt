bf4479deb2ce6d12b9d6d65a22227b8ec110021b
Giorgio Tosti Balducci <giorgio.tosti-balducci@pasqal.com>
2024-12-04 15:11:03 +0100

    Add HEA class

--- examples/psr_gradient.py
+++ examples/psr_gradient.py
@@ -1,40 +1,24 @@
 from __future__ import annotations
 
 import numpy as np
-from qiskit.circuit import ParameterVector, QuantumCircuit, QuantumRegister
 from qiskit.quantum_info import SparsePauliOp
 
+from physicslearningaqt.ansatz import HEA
 from physicslearningaqt.gradient import GradientPSR
 
 
 def main() -> None:
     rng = np.random.default_rng(seed=42)
+    n_qubits = 3
+    n_layers = 2
 
     g_psr = GradientPSR()
 
-    def hea(n_qubits: int, n_layers: int) -> QuantumCircuit:
-        params = ParameterVector("theta", n_qubits * n_layers)
-
-        qr = QuantumRegister(n_qubits)
-        qc = QuantumCircuit(qr)
-
-        for ll in range(n_layers):
-            for qq in range(n_qubits):
-                qc.rx(params[qq + ll * n_qubits], qq)
-            for q0, q1 in zip(range(n_qubits - 1), range(1, n_qubits)):
-                qc.cz(q0, q1)
-
-        qc.measure_all()
-
-        return qc
-
-    qc = hea(3, 2)
+    hea = HEA(n_qubits, n_layers)
 
     H = SparsePauliOp("Z" * 3)
 
-    params_values = rng.random((1, 6)) * 2 * np.pi
-
-    grad_values = g_psr.run(qc, H, params_values)
+    grad_values = g_psr.run(hea.circuit, H, hea.get_random_params(rng))
     print("State estimator gradient computed with parameter shift", grad_values)
 
 
---
+++ physicslearningaqt/ansatz.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import numpy as np
+from qiskit.circuit import ParameterVector, QuantumCircuit, QuantumRegister
+
+
+class HEA:
+    """Hardware Efficient Ansatz."""
+
+    def __init__(self, n_qubits: int, n_layers: int) -> None:
+        self._reg = QuantumRegister(n_qubits)
+        self._circ = QuantumCircuit(self._reg)
+        self._params = ParameterVector("theta", n_qubits * n_layers)
+
+        for ll in range(n_layers):
+            for qq in range(n_qubits):
+                self._circ.rx(self._params[qq + ll * n_qubits], qq)
+            for q0, q1 in zip(range(n_qubits - 1), range(1, n_qubits)):
+                self._circ.cz(q0, q1)
+
+        self._circ.measure_all()
+
+    @property
+    def circuit(self) -> QuantumCircuit:
+        return self._circ
+
+    def get_random_params(self, rng: np.random.Generator) -> np.ndarray:
+        return rng.random((1, len(self._params))) * 2 * np.pi
