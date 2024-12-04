edbf46f2a526d3d970cedb38954c021734898063
Giorgio Tosti Balducci <giorgio.tosti-balducci@pasqal.com>
2024-12-04 11:19:00 +0100

    Refactor gradient

--- physicslearningaqt/gradient.py
+++ physicslearningaqt/gradient.py
@@ -8,17 +8,38 @@ from qiskit_aqt_provider import AQTProvider
 from qiskit_aqt_provider.primitives import AQTEstimator
 
 
+# TODO convert to a pydantic BaseModel and include validation
+class GradientPSR:
+    def __init__(
+        self,
+        # TODO make backend name an Enum
+        backend_name: str = "offline_simulator_no_noise",
+        circuit_transpile_level: int = 3,
+    ) -> None:
+        """Sets the backend and estimator with the right transpile level for the circuit."""
+        self._backend = AQTProvider().get_backend(backend_name)
+
+        self._estimator = AQTEstimator(self._backend)
+        self._estimator.set_transpile_options(optimization_level=circuit_transpile_level)
+        self._gradient_estimator = ParamShiftEstimatorGradient(self._estimator)
+
+    def run(
+        self,
+        quantum_circuit: QuantumCircuit,
+        observable: SparsePauliOp,
+        parameter_values: np.ndarray,
+    ) -> np.ndarray:
+        return (
+            self._gradient_estimator.run(quantum_circuit, observable, parameter_values)
+            .result()
+            .gradients
+        )
+
+
 def main() -> None:
     rng = np.random.default_rng(seed=42)
 
-    backend_name = "offline_simulator_no_noise"
-
-    backend = AQTProvider().get_backend(backend_name)
-
-    estimator = AQTEstimator(backend)
-    estimator.set_transpile_options(optimization_level=3)  # embedded transpilation
-
-    gradient_estimator = ParamShiftEstimatorGradient(estimator)
+    g_psr = GradientPSR()
 
     def hea(n_qubits: int, n_layers: int) -> QuantumCircuit:
         params = ParameterVector("theta", n_qubits * n_layers)
@@ -42,7 +63,7 @@ def main() -> None:
 
     params_values = rng.random((1, 6)) * 2 * np.pi
 
-    grad_values = gradient_estimator.run(qc, H, params_values).result().gradients
+    grad_values = g_psr.run(qc, H, params_values)
     print("State estimator gradient computed with parameter shift", grad_values)
 
 
