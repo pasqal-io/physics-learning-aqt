351a64a8c61bf56c40fcc9b55e42499e108ae1e9
Giorgio Tosti Balducci <giorgio.tosti-balducci@pasqal.com>
2024-12-06 08:43:12 +0100

    Add test of GradientPSR validation

--- tests/test_gradient.py
+++ tests/test_gradient.py
@@ -1,1 +1,14 @@
 from __future__ import annotations
+
+import pytest
+from pydantic import ValidationError
+
+from physicslearningaqt.gradient import BackendName, GradientPSR
+
+
+@pytest.mark.parametrize("transpile_level", [-1, 4])
+def test_gradientpsr_wrong_transpile_level(transpile_level):
+    with pytest.raises(ValidationError):
+        GradientPSR(
+            backend_name=BackendName.OFFLINE_SIM_NO_NOISE, circuit_transpile_level=transpile_level
+        )
