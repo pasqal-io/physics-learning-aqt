e67755f288a0bae709e2318475f37f3c28de3b4a
Giorgio Tosti Balducci <giorgio.tosti-balducci@pasqal.com>
2024-12-06 14:48:20 +0100

    Add tests for HEA

--- tests/test_ansatz.py
+++ tests/test_ansatz.py
@@ -1,1 +1,16 @@
 from __future__ import annotations
+
+import numpy as np
+from qiskit.circuit import QuantumCircuit
+
+from physicslearningaqt.ansatz import HEA
+
+
+def test_hea():
+    rng = np.random.default_rng(42)
+    hea = HEA(n_qubits=3, n_layers=2)
+    assert isinstance(hea.circuit, QuantumCircuit)
+    assert hea.circuit.num_parameters == 6
+
+    rp = hea.get_random_params(rng)
+    assert rp.size == hea.circuit.num_parameters
